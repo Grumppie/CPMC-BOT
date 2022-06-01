8f0258c7ff841884578574291bf6cb3339ccbe6b
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-06-01 14:24:06 +0530

    all upcoming contests done

--- RIC.py
+++ RIC.py
@@ -11,38 +11,45 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-URL = "https://codeforces.com/api/contest.list?gym=false"
+URL_codeforces = "https://kontests.net/api/v1/codeforces"
+URL_codechef = "https://kontests.net/api/v1/code_chef"
+URL_leetcode = "https://kontests.net/api/v1/leet_code"
 
 # sending get request and saving the response as response object
-r = requests.get(url=URL)
+request_codeforces = requests.get(url=URL_codeforces)
+request_codechef = requests.get(url=URL_codechef)
+request_leetcode = requests.get(url=URL_leetcode)
 
 # extracting data in json format
-data = r.json()
-
-contests = data['result']
-
-
-def upcoming(contest):
-    if contest['phase'] == 'BEFORE':
-        return True
-    else:
-        return False
+data_codeforces = request_codeforces.json()
+data_codechef = request_codechef.json()
+data_leetcode = request_leetcode.json()
 
 
-def destructure(contest):
-    return [contest['name'], datetime.fromtimestamp(contest['startTimeSeconds']).strftime("%A, %B %d, %Y %I:%M:%S")]
+
+def destructure_codeforces_leetcode(contest):
+    date = contest['start_time'].split('T')[0]
+    time = contest['start_time'].split('T')[1]
+    return [contest['name'], str(date.split('-')[2] + "/" + date.split('-')[1] + "/" + date.split('-')[0] + " at " + str(
+        int(time[0:2:1]) + 6) + ":05")]
+
+def destructure_codechef(contest):
+    date = contest['start_time'].split(' ')[0]
+    time = contest['start_time'].split(' ')[1]
+    return [contest['name'], str(date.split('-')[2] + "/" + date.split('-')[1] + "/" + date.split('-')[0] + " at " + str(int(time[0:2:1]) + 6) + ":00")]
 
 
-contest_list = list(filter(upcoming, contests))
 
-display_list = list(map(destructure, contest_list))
+display_list = list(map(destructure_codeforces_leetcode, data_codeforces))[0:2:] + list(map(destructure_codechef,data_codechef))[0:2:] + list(map(destructure_codeforces_leetcode, data_leetcode))[0:2:]
+
 
 
 def display(display_list):
     dis = '\n**Upcoming Contests:**\n\n'
     i = 1
     for contest in display_list:
-        dis = dis + f'{i}) **Name**: {contest[0]}\n \t**Date**: {contest[1]}\n\n'
+        dis = dis + \
+              f'{i}) **Name**: {contest[0]}\n \t**Date**: {contest[1]}\n\n'
         i = i + 1
     return dis
 
